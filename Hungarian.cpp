f6b5e4c71c519b58308004bea1a9a17d2f7bd7e4
Nguyen Thinh <nguyenthinh@Nguyens-MacBook-Air-2.local>
2024-05-19 02:38:19 +0700

    Updated new way to resolve Hungarian Algorithms by Augument Path and Query Back

--- Hungarian.cpp
+++ Hungarian.cpp
@@ -1,203 +1,139 @@
-#include "Hungarian.h"
 #include <iostream>
-#include <limits>
-#include <algorithm>
+#include <Eigen/Dense>
+#include <vector>
+#include "Hungarian.h"
 
-HungarianAlgorithm::HungarianAlgorithm(const Eigen::MatrixXd& costMatrix)
-    : costMatrix_(costMatrix),
-      maskMatrix_(Eigen::MatrixXd::Zero(costMatrix.rows(), costMatrix.cols())),
-      rowCover_(costMatrix.rows(), false),
-      colCover_(costMatrix.cols(), false),
-      pathRow0_(-1),
-      pathCol0_(-1),
-      numRows_(costMatrix.rows()),
-      numCols_(costMatrix.cols()) {}
 
-Eigen::MatrixXd HungarianAlgorithm::reduceMatrix(const Eigen::MatrixXd& matrix) {
-    Eigen::MatrixXd reduced = matrix;
+HungarianAlgorithm::HungarianAlgorithm(Eigen::MatrixXd cost_matrix)
+{
+    this->_rows = cost_matrix.rows();
+    this->_cols = cost_matrix.cols();
+    this->_cost_matrix = cost_matrix;
+    this->_u = std::vector<int>(_rows + 1, 0);
+    this->_v = std::vector<int>(_cols + 1, 0);
+    this->_assignment = std::vector<int>(this->_cols + 1, 0);
+    this->_path = std::vector<int>(this->_cols + 1, 0);
+    this->_reduced_cost_matrix = Reduced_Cost_Matrix(cost_matrix);
+}
 
+void HungarianAlgorithm::solve(std::vector<int>& assignment, Eigen::MatrixXd &matching_assignment)
+{
+    Assigment_Optimize();
+    Extract_Assignment(assignment, matching_assignment);
+}
+
+Eigen::MatrixXd HungarianAlgorithm::Reduced_Cost_Matrix(Eigen::MatrixXd reduced_matrix)
+{
     // Reduce Rows
-    for (int i = 0; i < reduced.rows(); i++) {
-        reduced.row(i) -= Eigen::VectorXd::Constant(reduced.cols(), reduced.row(i).minCoeff());
+    for (int i = 0; i < reduced_matrix.rows(); i++) {
+        reduced_matrix.row(i) -= Eigen::VectorXd::Constant(reduced_matrix.cols(), reduced_matrix.row(i).minCoeff());
     }
 
     // Reduce Columns
-    for (int j = 0; j < reduced.cols(); j++) {
-        reduced.col(j) -= Eigen::VectorXd::Constant(reduced.rows(), reduced.col(j).minCoeff());
+    for (int j = 0; j < reduced_matrix.cols(); j++) {
+        reduced_matrix.col(j) -= Eigen::VectorXd::Constant(reduced_matrix.rows(), reduced_matrix.col(j).minCoeff());
     }
-    return reduced;
+    return reduced_matrix;
 }
 
-std::vector<int> HungarianAlgorithm::solve() {
-    // Step 1: Reduce the cost matrix
-    costMatrix_ = reduceMatrix(costMatrix_);
+void HungarianAlgorithm::Assigment_Optimize()
+{
+    std::cout << this->_reduced_cost_matrix << "\n";
+    for (int worker = 1; worker <= _rows; ++worker)
+    {
+        int cost = 0;
+        int assignedJob = 0;
+        std::vector<bool> visited(this->_cols + 1, false);
+        std::vector<int> min_Values(this->_cols + 1, INF);
+        this->_assignment[0] = worker;
 
-    // Steps 2-6: Execute the Hungarian algorithm
-    step2b();
-    bool done = false;
-    while (!done) {
-        step3();
-        done = step4();
-        if (!done) {
-            step5();
-            step6();
-        }
-    }
+        while (true)
+        {
+            visited[assignedJob] = true;
+            int delta = INF;
+            int next_Job = 0;
+            int currentWorker = _assignment[assignedJob];
 
-    // Extract the assignment
-    std::vector<int> assignment(numRows_, -1);
-    for (int i = 0; i < numRows_; ++i) {
-        for (int j = 0; j < numCols_; ++j) {
-            if (maskMatrix_(i, j) == 1) {
-                assignment[i] = j;
-                break;
-            }
-        }
-    }
-    return assignment;
-}
+            for ( int job = 1; job <= _cols; ++job)
+            {
+                if(!visited[job])
+                {
+                    cost = this->_reduced_cost_matrix(currentWorker - 1, job - 1) - this->_u[currentWorker] - this->_v[job];
 
-void HungarianAlgorithm::step2b() {
-    for (int i = 0; i < maskMatrix_.rows(); ++i) {
-        for (int j = 0; j < maskMatrix_.cols(); ++j) {
-            if (costMatrix_(i, j) == 0 && !rowCover_[i] && !colCover_[j]) {
-                maskMatrix_(i, j) = 1;
-                rowCover_[i] = true;
-                colCover_[j] = true;
-            }
-        }
-    }
-    rowCover_ = std::vector<bool>(numRows_, false);
-    colCover_ = std::vector<bool>(numCols_, false);
-}
+                    if ( cost < min_Values[job] )
+                    {
+                        min_Values[job] = cost;
+                        this->_path[job] = assignedJob;
+                    }
 
-void HungarianAlgorithm::step3() {
-    for (int i = 0; i < numRows_; ++i) {
-        for (int j = 0; j < numCols_; ++j) {
-            if (maskMatrix_(i, j) == 1) {
-                colCover_[j] = true;
-            }
-        }
-    }
-}
-
-bool HungarianAlgorithm::step4() {
-    int row = -1, col = -1;
-    bool done = false;
-    while (!done) {
-        for (int i = 0; i < numRows_; ++i) {
-            for (int j = 0; j < numCols_; ++j) {
-                if (costMatrix_(i, j) == 0 && !rowCover_[i] && !colCover_[j]) {
-                    row = i;
-                    col = j;
-                    maskMatrix_(i, j) = 2;
-                    if (std::find(maskMatrix_.col(j).data(), maskMatrix_.col(j).data() + numRows_, 1) != maskMatrix_.col(j).data() + numRows_) {
-                        int starRow = std::distance(maskMatrix_.col(j).data(), std::find(maskMatrix_.col(j).data(), maskMatrix_.col(j).data() + numRows_, 1));
-                        rowCover_[i] = true;
-                        colCover_[j] = false;
-                        row = starRow;
-                        break;
-                    } else {
-                        pathRow0_ = row;
-                        pathCol0_ = col;
-                        return false;
+                    if ( min_Values[job] < delta )
+                    {
+                        delta = min_Values[job];
+                        next_Job = job;
                     }
                 }
+                std::cout << "Job: " << job << std::endl;
+                std::cout << "Cost: " << this->_reduced_cost_matrix(currentWorker - 1, job - 1) << " - " << this->_u[currentWorker] << " - " << this->_v[job] << " = " << cost << std::endl;
+                std::cout << "Min Values[" << job << "]: " << min_Values[job] << std::endl;
+                std::cout << "Path[" << job << "]: " << this->_path[job] << std::endl;
             }
-            if (row != -1 && col != -1) break;
-        }
-        if (row == -1) done = true;
-    }
-    return done;
-}
 
-void HungarianAlgorithm::step5() {
-    bool done = false;
-    int count = 0;
-    path_.push_back({pathRow0_, pathCol0_});
-
-    while (!done) {
-        int row = -1, col = -1;
-        for (int i = 0; i < numRows_; ++i) {
-            if (maskMatrix_(i, path_.back().second) == 1) {
-                row = i;
+            std::cout << "Delta: " << delta << std::endl;
+            std::cout << "Next Job: " << next_Job << std::endl;
+            Update_Labels(delta, visited, min_Values);
+            assignedJob = next_Job;
+            
+            if (this->_assignment[assignedJob] == 0)
                 break;
-            }
         }
+        Augment_Path(assignedJob);
+        std::cout << "-----------------------------------------------------------\n";
+    }
+}
 
-        if (row != -1) {
-            path_.push_back({row, path_.back().second});
+void HungarianAlgorithm::Update_Labels(int delta, const std::vector<bool>& visited, std::vector<int>& minValues)
+{
+    std::cout << "vissted = [" << visited[0] << "-" << visited[1] << "-" << visited[2] << "-" << visited[3] << "]\n";
+    for( int job = 0; job <= this->_cols; job++)
+    {
+        if(!visited[job])
+        {
+            this->_u[job] += delta;
+            this->_v[job] -= delta;
         } else {
-            done = true;
+            minValues[job] -= delta;
         }
-
-        if (!done) {
-            for (int j = 0; j < numCols_; ++j) {
-                if (maskMatrix_(path_.back().first, j) == 2) {
-                    col = j;
-                    break;
-                }
-            }
-            path_.push_back({path_.back().first, col});
-        }
-    }
-
-    for (auto& p : path_) {
-        if (maskMatrix_(p.first, p.second) == 1) {
-            maskMatrix_(p.first, p.second) = 0;
-        } else if (maskMatrix_(p.first, p.second) == 2) {
-            maskMatrix_(p.first, p.second) = 1;
-        }
-    }
-
-    rowCover_ = std::vector<bool>(numRows_, false);
-    colCover_ = std::vector<bool>(numCols_, false);
-    for (int i = 0; i < numRows_; ++i) {
-        for (int j = 0; j < numCols_; ++j) {
-            if (maskMatrix_(i, j) == 2) {
-                maskMatrix_(i, j) = 0;
-            }
-        }
-    }
-    path_.clear();
-}
-
-void HungarianAlgorithm::step6() {
-    double minVal = std::numeric_limits<double>::max();
-    for (int i = 0; i < numRows_; ++i) {
-        for (int j = 0; j < numCols_; ++j) {
-            if (!rowCover_[i] && !colCover_[j]) {
-                if (costMatrix_(i, j) < minVal) {
-                    minVal = costMatrix_(i, j);
-                }
-            }
-        }
-    }
-
-    for (int i = 0; i < numRows_; ++i) {
-        for (int j = 0; j < numCols_; ++j) {
-            if (rowCover_[i]) costMatrix_(i, j) += minVal;
-            if (!colCover_[j]) costMatrix_(i, j) -= minVal;
-        }
+        
+        std::cout << "u[" << this->_assignment[job] << "]: " << this->_u[this->_assignment[job]] << std::endl;
+        std::cout << "v[" << job << "]: " << this->_v[job] << std::endl;
+        std::cout << "Min Values[" << job << "]: " << minValues[job] << std::endl;
     }
 }
-int main() {
-    Eigen::MatrixXd costMatrix(4, 4);
-    costMatrix << 82, 83, 69, 92,
-                  77, 37, 49, 92,
-                  11, 69, 5, 86,
-                   8, 9, 98, 23;
 
-    HungarianAlgorithm hungarian(costMatrix);
+void HungarianAlgorithm::Augment_Path(int assignedJob)
+{
+    while (true)
+    {
+        int previous_Job = this->_path[assignedJob];
+        this->_assignment[assignedJob] = this->_assignment[previous_Job];
+        assignedJob = previous_Job;
 
-    Eigen::MatrixXd reducedMatrix = hungarian.reduceMatrix(costMatrix);
-    std::cout << "Reduced Cost Matrix:\n" << reducedMatrix << std::endl;
+        std::cout << "Assigned Job: " << assignedJob << std::endl;
+        std::cout << "Previous Job: " << previous_Job << std::endl;
+        std::cout << "Assignment[" << assignedJob << "]: " << this->_assignment[assignedJob] << std::endl;
 
-    std::vector<int> assignment = hungarian.solve();
-    std::cout << "Assignments:" << std::endl;
-    for (size_t i = 0; i < assignment.size(); ++i) {
-        std::cout << "Row " << i << " assigned to Column " << assignment[i] << std::endl;
+        if (assignedJob == 0) break;
     }
+}
 
-    return 0;
+void HungarianAlgorithm::Extract_Assignment(std::vector<int> &assignment_results, Eigen::MatrixXd &matching_assignment)
+{
+    assignment_results.resize(this->_rows);
+    matching_assignment = Eigen::MatrixXd::Constant(this->_rows, this->_cols, 0);
+    for (int job = 1; job <= this->_cols; job++)
+    {
+        assignment_results[this->_assignment[job] - 1] = job - 1;
+        matching_assignment(this->_assignment[job] - 1, job - 1) = 1;
+        std::cout << "Final Assignment - Worker: " << this->_assignment[job] - 1 << ", Job: " << job - 1 << std::endl;
+    }
 }
--- Hungarian.h
+++ Hungarian.h
@@ -4,27 +4,26 @@
 #include <Eigen/Dense>
 #include <vector>
 
+const int INF = std::numeric_limits<int>::max();
+
 class HungarianAlgorithm {
 public:
-    HungarianAlgorithm(const Eigen::MatrixXd& costMatrix);
-    Eigen::MatrixXd reduceMatrix(const Eigen::MatrixXd& matrix);
-    std::vector<int> solve();
+    HungarianAlgorithm(const Eigen::MatrixXd cost_matrix);
+    // HungarianAlgorithm();
+    void solve(std::vector<int>& assignment, Eigen::MatrixXd &matching_assignment);
 
 private:
-    void step2b();
-    void step3();
-    bool step4();
-    void step5();
-    void step6();
+    Eigen::MatrixXd _cost_matrix;
+    Eigen::MatrixXd _reduced_cost_matrix;
+    std::vector<int> _u, _v, _assignment, _path;
+    int _rows, _cols;
 
-    Eigen::MatrixXd costMatrix_;
-    Eigen::MatrixXd maskMatrix_;
-    std::vector<bool> rowCover_;
-    std::vector<bool> colCover_;
-    std::vector<std::pair<int, int>> path_;
-    int pathRow0_, pathCol0_;
-    int numRows_;
-    int numCols_;
+    void Assigment_Optimize();
+    void Update_Labels(int delta, const std::vector<bool>& visited, std::vector<int>& minValues);
+    void Augment_Path(int assignedJob);
+    void Extract_Assignment(std::vector<int> &assignment_results, Eigen::MatrixXd &matching_assignment);
+    Eigen::MatrixXd Reduced_Cost_Matrix(Eigen::MatrixXd cost_matrix);
+
 };
 
 #endif // HUNGARIAN_H
